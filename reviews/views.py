03d5695ae3d087ad63a670004224e72fbf2f3772
Netik Viacheslav <slava5582@gmail.com>
2023-08-02 14:50:58 +0200

    Adaptive and add new classes for view.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,30 +1,33 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 from .forms import ReviewForm
 from .models import Review
 
 
-# Create your views here.
-
-def review(request):
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating'])
-            review.save()
-            return HttpResponseRedirect('/thank-you')
-    else:
-        form = ReviewForm()
-
-    return render(request, 'reviews/review.html', {
-        'form': form
-    })
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/review.html'
+    success_url = '/thank_you'
 
 
-def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
+class ThankYouView(TemplateView):
+    template_name = 'reviews/thank_you.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = "This works!"
+        return context
+
+
+class ReviewsListView(ListView):
+    template_name = 'reviews/reviews_list.html'
+    model = Review
+    context_object_name = 'reviews'
+
+
+class SingleReviewView(DetailView):
+    template_name = 'reviews/single_review.html'
+    model = Review
